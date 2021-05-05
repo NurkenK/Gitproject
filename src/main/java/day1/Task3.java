f157d469b4340686db1a3619db81380b1e70d2b1
Nurken Khabibullayev <mr.nur21@gmail.com>
2021-05-06 03:58:11 +0600

    day1 task6, requesting support

---
+++ src/main/java/day1/Task3.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        for(int g = 0; g<10; g++) {
+            System.out.println("JAVA");
+        }
+
+    }
+}
---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        for( int k = 5; k>0 && k<10; k++)
+        System.out.println(k);
+        int 1 * k = k;
+
+
+    }
+}
